3d34565b240f67c47131e43b2b9d529ba87914ac
sergey-popov-1964 <sergeykms@gmail.com>
2022-06-08 20:01:20 +0700

    08_06_2022__20_01

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
 @@include('../components/header/header.js')
 @@include('../components/offer/offer.js')
 @@include('../components/about/about.js')
+@@include('../components/stack/stack.js')
 
 //Слушатель изменения ориентации телефона/планшета
 //При изменении - обновление
